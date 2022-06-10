eeb5cfb9baf1941f0ab0f00a20f6b646bd33d01f
Alan Joseph <alan@sapaad.com>
2022-06-10 18:38:43 +0530

    Dashboard tests

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sapaad BlackBox
+
+All in one black box test engine for Sapaad Services
---
+++ tests/dashboard_2_0/sales_dashboard.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('Sales Dashboard @dashboard', async ({ page }) => {
+
+  // Go to https://sapaad-reporting-ms-review.herokuapp.com/
+  await page.goto('https://sapaad-reporting-ms-review.herokuapp.com/');
+
+  // Click text=Sales
+  await page.locator('text=Sales').click();
+  await expect(page).toHaveURL('https://sapaad-reporting-ms-review.herokuapp.com/sales');
+
+  // Make sure the list now has two todo items.
+  await expect(page.locator('h1')).toHaveText('Sales');
+});
